d76b094d4864142e87746b8b6eb85c252f418705
Zachary Wichman <wichmaz@wwu.edu>
2021-05-25 15:03:42 -0700

    Added the mapbox map

--- night-sky-satellite-viewer/app/src/main/java/com/example/nightskysatelliteviewer/MainActivity.kt
+++ night-sky-satellite-viewer/app/src/main/java/com/example/nightskysatelliteviewer/MainActivity.kt
@@ -7,7 +7,143 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import java.io.File
 import java.net.URL
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.PointF
+import android.widget.Toast
+import androidx.annotation.NonNull
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
+import com.mapbox.mapboxsdk.style.layers.Layer
+import com.mapbox.mapboxsdk.style.layers.Property
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory.*
+import java.util.*
 
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var mapView: MapView? = null
+    private lateinit var map: MapboxMap
+    private lateinit var map_style: Style
+    private var allSats = arrayListOf<mySatellite>()
+    private var labelsize: Float = 15.0F
+    private var stateLabelSymbolLayer: Layer? = null
+
+    val LAYER_ID = "MAIN"
+    val SOURCE_ID = "SAT_DB"
+    val ICON_ID = "SAT"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+
+        setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+
+        mapView?.getMapAsync(this)
+
+        allSats.add(mySatellite("Bellingham", "bham_id", LatLng(48.747789, -122.479255)))
+        allSats.add(mySatellite("Japan", "jpn_id", LatLng(35.478780, 137.472501)))
+
+        val refresh_button: FloatingActionButton = findViewById(R.id.reload)
+        refresh_button.setOnClickListener {
+            mapView!!.refreshDrawableState()
+        }
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        map = mapboxMap
+        mapboxMap.setStyle(Style.Builder().fromUri(Style.MAPBOX_STREETS)
+            .withImage(ICON_ID, BitmapFactory.decodeResource(
+                this.getResources(), R.drawable.sat)),
+            object : Style.OnStyleLoaded {
+                override fun onStyleLoaded(@NonNull style: Style) {
+                    map_style = style
+                    stateLabelSymbolLayer = style.getLayer("state-label");
+
+                    val symbolManager = SymbolManager(mapView!!, mapboxMap, style)
+                    symbolManager.setIconAllowOverlap(true)
+                    symbolManager.setTextAllowOverlap(true)
+
+                    for (item in allSats) {
+                        var id = item.id
+                        var loc = item.loc
+                        var name = item.name
+
+                        val symbol = symbolManager.create(SymbolOptions()
+                            .withLatLng(loc)
+                            .withIconImage(ICON_ID)
+                            .withIconSize(.50f))
+                        symbol.textField = name
+                        symbol.textSize = labelsize
+                        symbol.textOffset = PointF(2f, 2f)
+                        symbolManager.update(symbol)
+
+                        symbolManager.addClickListener { symbol ->
+                            toast { "You clicked the " + symbol.textField + " satellite!" }
+                            true
+
+                        }
+                    }
+                    stateLabelSymbolLayer!!.setProperties(
+                        textIgnorePlacement(true),
+                        textAllowOverlap(true),
+                        textAnchor(Property.TEXT_ANCHOR_BOTTOM))
+                }
+            })
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+    inline fun Context.toast(message: () -> String) {
+        Toast.makeText(this, message(), Toast.LENGTH_LONG).show()
+    }
+}
+
+/***************************************************************8
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,3 +184,4 @@ class MainActivity : AppCompatActivity() {
         converter.getLongitude(testSat)
     }
 }
+*/
--- night-sky-satellite-viewer/app/src/main/java/com/example/nightskysatelliteviewer/Satellite.kt
+++ night-sky-satellite-viewer/app/src/main/java/com/example/nightskysatelliteviewer/Satellite.kt
@@ -1,3 +1,7 @@
 package com.example.nightskysatelliteviewer
 
+import com.mapbox.mapboxsdk.geometry.LatLng
+
+
+class mySatellite(val name: String, val id: String, val loc: LatLng)
 class Satellite(val name: String, val id: String, val epoch: Long) { }
